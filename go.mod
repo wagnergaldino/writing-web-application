2af5379d3602dad3f4db7c32911daebfbf07f3f7
“Wagner” <wagalwagal@gmail.com>
2023-05-31 17:47:37 -0300

    Exercicios adicionais sugeridos

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wagnergaldino/writing-web-application
+
+go 1.20
--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,35 +1,78 @@
 package main
 
 import (
-	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 type Page struct {
 	Title string
+	Menu  template.HTML
 	Body  []byte
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html", "tmpl/frontpage.html"))
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := "data/" + p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := "data/" + title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	menu, err := getMenu(title)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Menu: template.HTML(menu), Body: body}, nil
+}
+
+func getMenu(title string) (string, error) {
+	var ret string
+
+	p, err := filepath.Abs("./wiki.go")
+
+	if err != nil {
+		return ret, err
+	}
+
+	f, err := os.Open(filepath.Dir(p) + "/data")
+	if err != nil {
+		return ret, err
+	}
+	files, err := f.Readdir(0)
+	if err != nil {
+		return ret, err
+	}
+
+	for _, v := range files {
+		if ret != "" {
+			ret += " | "
+		}
+
+		filename := strings.Replace(v.Name(), ".txt", "", 1)
+		if filename != title {
+			ret += "<a href='/view/" + filename + "'>" + filename + "</a>"
+		} else {
+			ret += filename
+		}
+	}
+	return ret, err
+}
+
+func frontpageHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -67,15 +110,6 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
-}
-
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
@@ -88,6 +122,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 }
 
 func main() {
+	http.HandleFunc("/", frontpageHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
